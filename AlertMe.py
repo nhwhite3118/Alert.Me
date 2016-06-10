be49b8b265b680dad76a8c741d46ba7309c9dc43
Nick White <britishnickk@gmail.com>
2016-06-10 16:56:24 -0500

    Gmail no longer blocks alert emails with urls

--- AlertMe.py
+++ AlertMe.py
@@ -5,64 +5,31 @@ import pymysql
 import time
 import smtplib
 
-# We need to keep track of the number of unique cities we ask weatherunderground
-# for. We get 10 API calls/minute, and 500/day, so we need to keep totals
-wundergroundThisDay = 0
-# We need to cache cities so that our usage scales with unique cities instead
-# of unique users
-forecasts = {}
-conn = pymysql.connect(host="127.0.0.1", user=config.DB_USER, passwd=config.DB_PASSWORD, db="thunderbuddy")
+conn = pymysql.connect(host="127.0.0.1", user=config.DB_USER, passwd=config.DB_PASSWORD, db="alertme")
 
 
 def sendAlerts():
     print("time - " + time.ctime())
-    wundergroundThisMinute = 0
     cur = conn.cursor()
     cur.execute("SELECT * FROM user")
 
     for user in cur:
         number = user[0]
-        city = user[1]
-        state = user[2]
-        portal = user[3]
-        if city + state not in forecasts:
-            # find forecasts only for new cities
-            print("Checking forecast for " + city)
-            wundergroundThisMinute += 1
-            if wundergroundThisMinute >= 10:
-                # Only make 10 api calls/min
-                time.sleep(120)
-                wundergroundThisMinute = 0
-            forecasts[city + state] = makeForecast(city, state)
-
-        if len(forecasts[city + state]) > 0:
-            # if there is thunder, message the user
-            sendEmailSms(number + "@" + portal, forecasts[city + state])
+        portal = user[1]
+        match = user[2]
+        site = user[3]
+        notSpam = site.replace("http://","").replace("https://","").replace("www.","")
+        if checkSite(match,site):
+        sendEmailSms(number + "@" + portal, "Your AlertMe alert for "+match+" at " + str(notSpam)+ " has gone off!")
 
 
-def makeForecast(city, state):
-    # Retrieve Weather Underground Data
-    f = urlopen("http://api.wunderground.com/api/" + config.WEATHER_UNDERGROUND_API_KEY + "/forecast/q/" + state + "/" + city + ".json")
-    jsonString = f.read().decode('utf-8')
-    parsedJson = json.loads(jsonString)
-    forecastList = parsedJson["forecast"]["txt_forecast"]["forecastday"]
-    thunderDays = []
-
-    message = ""
-    for forecastEntry in forecastList:
-        fcttext = forecastEntry["fcttext"].lower()
-        fcttextMetric = forecastEntry["fcttext_metric"].lower()
-        if "thunder" in fcttext or "thunder" in fcttextMetric:
-            thunderDays.append(forecastEntry["title"])
-
-    if len(thunderDays) > 0:
-        # build message and replace last comma with 'and'
-        message = "Thunder coming on " + ", ".join(thunderDays)[::-1].replace(",", "dna ", 1)[::-1]
-
+def checkSite(match,site):
+    f = urlopen(site)
+    siteString = f.read().decode('utf-8')
     f.close()
-    return message
+    return match in siteString
 
-
+#TODO: make alertme email
 def sendEmailSms(recipient, body):
     gmailUser = 'thunderbuddyproject@gmail.com'
     gmailPassword = config.EMAIL_PASSWORD
